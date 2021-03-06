2c1b910ca4b8411fc7133aeb0f73f40821cd5552
Furkan Paşaoğlu <16624085+furkanpasaoglu@users.noreply.github.com>
2021-03-07 00:03:29 +0300

    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './componnents/product/product.component';
+import { CategoryComponent } from './componnents/category/category.component';
+import { NaviComponent } from './componnents/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
